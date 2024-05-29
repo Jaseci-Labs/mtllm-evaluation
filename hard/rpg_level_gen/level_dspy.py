c90f6f3c55d234958a7c76419dcfbe3adf74a6bd
chandralegend <irugalbandarachandra@gmail.com>
2024-05-30 03:40:03 +0530

    chore: Remove redundant code for getting next level in level.jac and level_dspy.py

--- hard/rpg_level_gen/level_dspy.py
+++ hard/rpg_level_gen/level_dspy.py
@@ -111,7 +111,6 @@ def get_map(map: Map) -> List[str]:
 if __name__ == "__main__":
     level_manager = LevelManager()
     for _ in range(2):
-        level_manager.get_next_level()
         new_level, new_level_map = level_manager.get_next_level()
         print(new_level)
         print("\n".join(get_map(new_level_map)))
