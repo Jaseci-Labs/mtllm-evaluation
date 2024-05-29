48e7950214f83fb4c9edc64bb246746f9c2ef74d
chandralegend <irugalbandarachandra@gmail.com>
2024-05-29 07:54:25 +0530

    chore: Refactor and clean-up code, update Wikipedia module for better question answering

---
+++ hard/wikipedia/wikipedia_react_dspy.py
@@ -0,0 +1,31 @@
+import dspy
+import wikipedia
+from dspy.predict.parameter import Parameter
+
+llm = dspy.OllamaLocal(base_url="http://52.23.242.52:11343", model="phi3")
+dspy.settings.configure(lm=llm)
+
+
+class Wikipedia(Parameter):
+    name = "Search"
+    input_variable = "query"
+    desc = "takes a search query and returns summary from wikipedia"
+
+    def __init__(self, k=3) -> None:
+        pass
+
+    def __call__(self, query):
+        return wikipedia.summary(query)
+
+
+get_answer = dspy.ReAct("question -> answer", tools=[Wikipedia()])
+
+
+question = "Where is Apple Headquaters located?"
+result = get_answer(question=question)
+print("Question: ", question)
+print("Answer: ", result.answer)
+question = "Who is Jason Mars?"
+result = get_answer(question=question)
+print("Question: ", question)
+print("Answer: ", result.answer)
