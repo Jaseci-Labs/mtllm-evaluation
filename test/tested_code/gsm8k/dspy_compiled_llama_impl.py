97136586b0fb9dc61085b70095577c8ccb91117f
Patrick Li <baichuanli@yahoo.com>
2024-09-12 01:53:04 -0400

    fix: add hostname

--- test/tested_code/gsm8k/dspy_compiled_llama_impl.py
+++ test/tested_code/gsm8k/dspy_compiled_llama_impl.py
@@ -3,7 +3,7 @@ import os
 import dspy
 
 model_name = os.environ["MODEL_NAME"]
-llm = dspy.OllamaLocal(model=model_name)
+llm = dspy.OllamaLocal(model=model_name, base_url="http://141.212.106.90:8080")
 dspy.configure(lm=llm)
 program = CoT
 CoT.load("optimized.json")
