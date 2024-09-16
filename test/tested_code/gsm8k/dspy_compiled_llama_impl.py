c893c1d87c29c7e9bed614bb87bf977d52cf47a1
Patrick Li <patrli@umich.edu>
2024-09-16 17:27:51 -0400

    fmt

--- test/tested_code/gsm8k/dspy_compiled_llama_impl.py
+++ test/tested_code/gsm8k/dspy_compiled_llama_impl.py
@@ -3,7 +3,9 @@ import os
 import dspy
 
 model_name = os.environ["MODEL_NAME"]
-llm = dspy.OllamaLocal(model=model_name, base_url="http://141.212.106.90:8080", timeout_s=1200)
+llm = dspy.OllamaLocal(
+    model=model_name, base_url="http://141.212.106.90:8080", timeout_s=1200
+)
 dspy.configure(lm=llm)
 program = CoT
 CoT.load("optimized.json")
